ea20de6e50b369015e169174c5fbac927634b665
dev-vicky-29 <150275960+dev-vicky-29@users.noreply.github.com>
2024-06-24 22:28:39 +0530

    Create index.js

---
+++ src/constants/index.js
@@ -0,0 +1,79 @@
+import project1 from "../assets/projects/To-Do App.png";
+import project2 from "../assets/projects/Weather App.png";
+import project3 from "../assets/projects/Tic-Tac-Toe.png";
+import project4 from "../assets/projects/Calculator.png";
+import project5 from "../assets/projects/CRUD Application.png";
+import project6 from "../assets/projects/Portfolio.png";
+
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `With over 2 years of comprehensive experience in the field of Web Development & Software Development, accompanied by a bachelor's degree in Computer Science. Proficient in Java Development, Web Development, Unit Testing, SDLC, & machine learning. Demonstrated success in leading impactful projects and providing effective mentorship.`;
+
+export const EXPERIENCES = [
+  // {
+  //   year: "July 2024 - Present",
+  //   role: "MERN Stack Developer Intern",
+  //   company: "Nezuware",
+  //   description: `During my internship as a MERN Stack Developer at Nezuware, I had the opportunity to work on diverse and challenging projects that enhanced my skills in full-stack web development. My role involved collaborating with a team of experienced developers to design, develop, and deploy web applications using the MERN (MongoDB, Express.js, React, Node.js) stack.`,
+  //   technologies: ["Javascript", "React.js", "Express.js", "Node.js", "mongoDB"],
+  // },
+  {
+    year: "April 2024-May 2024",
+    role: "Java Programming Internship",
+    company: "INTERNPE",
+    description: `As an intern, I was responsible for developing game applications, implementing Java programming concepts, and refining my problem-solving skills through practical tasks. Utilized object-oriented programming principles, control flow mechanisms, and data structures to create robust and efficient code for each project.`,
+    technologies: ["Java", "OPPs", "VS Code"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Portfolio Website",
+    image: project6,
+    description:
+      "Created a personal portfolio website showcasing my skills, projects, and professional experience as a Web Developer.",
+    technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+  },
+  {
+    title: "To-Do App",
+    image: project1,
+    description:
+      "Developed a user-friendly To-Do App to organize tasks by category, set priorities, and track due dates.Built using HTML, CSS, and JavaScript, the app features a responsive design, category selection, priority settings, and due date management.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "Weather App",
+    image: project2,
+    description:
+      "Designed to provide real-time weather updates based on user location input. This application fetches and displays current temperature, weather conditions, humidity, and wind speed with a user-friendly interface. It demonstrates my proficiency in front-end development and API integration.",
+    technologies: ["HTML", "CSS",  "JavaScript"],
+  },
+  {
+    title: "Tic-Tic-Toe App",
+    image: project3,
+    description:
+      "The game allows two players to take turns marking spaces on a 3x3 grid, with the game declaring a winner once a player achieves three marks in a row, column, or diagonal.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "Calculator",
+    image: project4,
+    description:
+      "The calculator provides essential arithmetic operations including addition, subtraction, multiplication, and division, with a clean and intuitive user interface.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "CRUD Application",
+    image: project5,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML",  "Bootstrap", "JavaScript"],
+  },
+];
+
+export const CONTACT = {
+  address: "Satara, Maharashtra, India ",
+  phoneNo: "+91 95032 65702",
+  email: "vivekdeshmukh2509@gmail.com",
+};
